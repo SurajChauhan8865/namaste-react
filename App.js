f7ae4b4e67a829a047e288975ff617b431bfc40b
suraj chauhan <suraj@cloudsanalytics.ai>
2025-03-04 15:55:18 +0530

    tutorial-2

--- App.js
+++ App.js
@@ -1,42 +1,5 @@
-// const heading = React.createElement('h1', { id: "heading", xyz: "abc" }, 'Hello world! From React');
-// console.log(heading);
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// console.log(root)
-// root.render(heading);
-
-// /**
-//  * <div id='parent'>
-//  *    <div id='child'>
-//  *        <h1> I am h1 tag </h1>
-//  *        <h2> I am h2 tag </h2>
-//  *    </div>
-//  * <div>
-//  */
-
-// const parent = React.createElement(
-//   'div',
-//   { id: 'parent' },
-//   React.createElement('div', { id: 'child' }, [
-//     React.createElement('h1', {}, 'I am h1 tag'),
-//     React.createElement('h2', {}, 'I am h2 tag')
-//   ]
-//   )
-// )
-
-// console.log(parent)
-
-/**
- * <div id='parent'>
- *    <div id='child'>
- *        <h1> I am h1 tag </h1>
- *        <h2> I am h2 tag </h2>
- *    </div>
- *    <div id='child2'>
- *        <h1> I am h1 tag </h1>
- *        <h2> I am h2 tag </h2>
- *    </div>
- * <div>
- */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement(
   'div',
@@ -53,8 +16,6 @@ const parent = React.createElement(
   )
 ]
 )
-
-console.log(parent)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Namaste React by suraj
+
+
+# Parcel
+
+- Dev Build
+- Local server 
+- HMR = Hot Module Replacement
+- File watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hasing
+- Code Splitting
+- Differential Bundling - support old browsers
+- Diagnostic 
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different Development and Production Bundles
